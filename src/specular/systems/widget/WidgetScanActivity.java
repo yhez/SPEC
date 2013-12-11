bb1d4d1d851cc7cdca5fc40664f202170a6042d8
yhez <yhezkel88@gmail.com>
2013-12-11 15:48:53 +0200

    widget scanner is now fully working

--- src/specular/systems/widget/WidgetScanActivity.java
+++ src/specular/systems/widget/WidgetScanActivity.java
@@ -1,42 +1,41 @@
 package specular.systems.widget;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.widget.TextView;
 
 import com.google.zxing.Result;
 
-import specular.systems.CryptMethods;
-import specular.systems.Dialogs.ProgressDlg;
-import specular.systems.FilesManagement;
-import specular.systems.PublicContactCard;
-import specular.systems.PublicStaticVariables;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
 import specular.systems.R;
+import specular.systems.activitys.Splash;
 import specular.systems.scanqr.CaptureActivity;
 
 /**
  * Created by yehezkelk on 12/11/13.
  */
 public class WidgetScanActivity extends CaptureActivity {
+
     @Override
-    public void handleDecode(final Result rawResult) {
-        if(rawResult!=null){
-        PublicContactCard pcc = new PublicContactCard(this,rawResult.getText());
-        if(pcc.getPublicKey()!=null){
-            //todo add contact
-        }else {
-            final ProgressDlg prgd = new ProgressDlg(this, R.string.decrypting);
-            prgd.setCancelable(false);
-            prgd.show();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    CryptMethods.decrypt(rawResult.getText());
-                    FilesManagement.deleteTempDecryptedMSG(WidgetScanActivity.this);
-                    PublicStaticVariables.message = null;
-                    //todo show decrypted msg
-                    prgd.cancel();
-                }
-            }).start();
-        }
+    public void handleDecode(Result rawResult) {
+        if (rawResult != null) {
+            try {
+                File path = getFilesDir();
+                File file = new File(path, "temp_open_by_widget");
+                OutputStream os = new FileOutputStream(file);
+                os.write(rawResult.getText().getBytes());
+                os.close();
+                Intent i = new Intent(this, Splash.class);
+                i.setAction(Intent.ACTION_VIEW);
+                i.setData(Uri.fromFile(file));
+                finish();
+                startActivity(i);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
