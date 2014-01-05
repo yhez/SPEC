cebe9cee2c824869b49b9697c22c39a0a6a480c6
oritro <oritroyna@gmail.com>
2014-01-05 22:10:24 +0200

    Backup story
    user can backup his public information and contacts

--- app/src/main/java/specular/systems/Backup.java
+++ app/src/main/java/specular/systems/Backup.java
@@ -9,25 +9,24 @@ import java.util.List;
  * Created by yehezkelk on 12/28/13.
  */
 public class Backup {
-    public static final String DIVIDE_FIELDS ="\n";
-    public static void backup(Activity a){
-        List<Contact> contactList = ContactsDataSource.contactsDataSource.getAllContacts();
+    public static final String Delimiter ="\n";
+    public static boolean backup(Activity a){
+        List<Contact> contactList = StaticVariables.contactsDataSource.getAllContacts();
         String[] myDetails = CryptMethods.getMyDetails(a);
-        String details = myDetails[0]+ DIVIDE_FIELDS+myDetails[1]+DIVIDE_FIELDS+myDetails[2]+DIVIDE_FIELDS;
+        String details = myDetails[0]+ Delimiter+myDetails[1]+Delimiter+myDetails[2]+Delimiter;
         byte[] my_details;
         try {
-           my_details = details.getBytes("UTF-8");
+            my_details = details.getBytes("UTF-8");
         } catch (UnsupportedEncodingException e) {
             my_details = details.getBytes();
         }
+        int size=my_details.length;
         byte[][] contacts = new byte[contactList.size()][];
         for(int b=0;b<contactList.size();b++){
             contacts[b]= contact2Bytes(contactList.get(b));
-        }
-        int size=my_details.length;
-        for(int b=0;b<contacts.length;b++){
             size+=contacts[b].length;
         }
+
         byte[] finalResult= new byte[size];
         System.arraycopy(my_details,0,finalResult,0,my_details.length);
         int location = my_details.length;
@@ -38,20 +37,26 @@ public class Backup {
         //byte[] encrypytedData = CryptMethods.encrypt(finalResult,null,CryptMethods.getPublic());
         //FilesManagement.saveBackupFile(encrypytedData);
         //show dialog with the option to upload the file to some place
+        return FilesManagement.createBackupFileToSend(a, finalResult);
     }
+
+    public static void restore(Activity a){
+
+    }
+
     private static byte[] contact2Bytes(Contact c){
-        String contacts="";
-        contacts += c.getContactName()+ DIVIDE_FIELDS;
-        contacts += c.getEmail()+ DIVIDE_FIELDS;
-        contacts += c.getPublicKey()+ DIVIDE_FIELDS;
-        contacts += c.getSession()+ DIVIDE_FIELDS;
-        contacts += c.getAdded()+ DIVIDE_FIELDS;
-        contacts += c.getReceived()+ DIVIDE_FIELDS;
-        contacts += c.getSent()+ DIVIDE_FIELDS;
+        String contact="";
+        contact += c.getContactName()+ Delimiter;
+        contact += c.getEmail()+ Delimiter;
+        contact += c.getPublicKey()+ Delimiter;
+        contact += c.getSession()+ Delimiter;
+        contact += c.getAdded()+ Delimiter;
+        contact += c.getReceived()+ Delimiter;
+        contact += c.getSent()+ Delimiter;
         try {
-            return contacts.getBytes("UTF-8");
+            return contact.getBytes("UTF-8");
         } catch (UnsupportedEncodingException e) {
-            return contacts.getBytes();
+            return contact.getBytes();
         }
     }
 }
