029d0a9c68a523d2568f934dadcb1b8de4cfea7a
yhez <yhezkel88@gmail.com>
2014-04-15 19:33:24 +0300

    removed drive implementation
    reason: not easy for user to share the file on drive

--- app/src/main/java/specular/systems/activities/GoogleDriveActivity.java
+++
@@ -1,7 +0,0 @@
-package specular.systems.activities;
-
-/**
- * Created by yehezkelk on 4/14/2014.
- */
-public class GoogleDriveActivity {
-}
--- app/src/main/java/specular/systems/activities/SendMsg.java
+++ app/src/main/java/specular/systems/activities/SendMsg.java
@@ -3,13 +3,11 @@ package specular.systems.activities;
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.content.IntentSender;
 import android.content.pm.ResolveInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.text.Html;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
@@ -19,19 +17,9 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.drive.Drive;
-import com.google.android.gms.drive.DriveApi;
-import com.google.android.gms.drive.MetadataChangeSet;
-
 import java.io.File;
 import java.io.FilenameFilter;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,13 +31,13 @@ import specular.systems.Group;
 import specular.systems.GroupDataSource;
 import specular.systems.KeysDeleter;
 import specular.systems.R;
-import specular.systems.StaticVariables;
 import specular.systems.Visual;
 
 import static android.support.v4.content.FileProvider.getUriForFile;
 
-public class SendMsg extends Activity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+
+@SuppressWarnings("ResultOfMethodCallIgnored")
+public class SendMsg extends Activity {
     public static boolean msgSended;
     public static final int CONTACT = 1, MESSAGE = 2, INVITE_GROUP = 3, MESSAGE_FOR_GROUP = 4, BACKUP = 5, REPORT = 6, OPEN_FILE = 7;
     private static List<ResolveInfo> file;
@@ -119,16 +107,16 @@ public class SendMsg extends Activity implements GoogleApiClient.ConnectionCallb
         setContentView(R.layout.send_msg_dlg);
         updateViews();
         if (uris != null) {
-                if (type == BACKUP)
-                    ((TextView) findViewById(R.id.file_size)).setText(Visual.getSize(new File(getFilesDir() + FilesManagement.MESSAGES, getString(R.string.file_name_Backup_msg)).length()).replace(" ", Visual.strings.NEW_LINE));
-                else
-                    ((TextView) findViewById(R.id.file_size)).setText(Visual.getSize(new File(getFilesDir() + FilesManagement.MESSAGES, getString(R.string.file_name_secure_msg)).length()).replace(" ", Visual.strings.NEW_LINE));
-                ((ImageView) findViewById(R.id.file_icon)).setImageResource(R.drawable.logo);
-                EditText etFile = (EditText) findViewById(R.id.name_file);
-                etFile.setText(getName());
-                etFile.setSelection(etFile.length());
-                etFile.setFilters(Visual.filters());
-            }
+            if (type == BACKUP)
+                ((TextView) findViewById(R.id.file_size)).setText(Visual.getSize(new File(getFilesDir() + FilesManagement.MESSAGES, getString(R.string.file_name_Backup_msg)).length()).replace(" ", Visual.strings.NEW_LINE));
+            else
+                ((TextView) findViewById(R.id.file_size)).setText(Visual.getSize(new File(getFilesDir() + FilesManagement.MESSAGES, getString(R.string.file_name_secure_msg)).length()).replace(" ", Visual.strings.NEW_LINE));
+            ((ImageView) findViewById(R.id.file_icon)).setImageResource(R.drawable.logo);
+            EditText etFile = (EditText) findViewById(R.id.name_file);
+            etFile.setText(getName());
+            etFile.setSelection(etFile.length());
+            etFile.setFilters(Visual.filters());
+        }
         Visual.setAllFonts(this, (ViewGroup) findViewById(android.R.id.content));
     }
 
@@ -146,24 +134,27 @@ public class SendMsg extends Activity implements GoogleApiClient.ConnectionCallb
                     file.remove(a);
                     break;
                 }
-        }else {
+        } else {
             intent = new Intent(Intent.ACTION_SEND);
             intent.setType("file/*");
             file = getPackageManager().queryIntentActivities(intent, 0);
+            ResolveInfo drive=null,me=null;
             for (int a = 0; a < file.size(); a++)
-                if (file.get(a).activityInfo.packageName.equals(getPackageName())) {
-                    file.remove(a);
-                    break;
-                }
+                if (file.get(a).activityInfo.packageName.equals(getPackageName()))
+                    me = file.get(a);
+                else if(file.get(a).activityInfo.packageName.equals("com.google.android.apps.docs"))
+                    drive = file.get(a);
+            if(me!=null) file.remove(me);
+            if(drive!=null)file.remove(drive);
         }
-}
-
-final FilenameFilter filterNotReported = new FilenameFilter() {
-    @Override
-    public boolean accept(File file, String s) {
-        return s.endsWith(".stacktrace");
     }
-};
+
+    final FilenameFilter filterNotReported = new FilenameFilter() {
+        @Override
+        public boolean accept(File file, String s) {
+            return s.endsWith(".stacktrace");
+        }
+    };
 
     private void startOnClick(ResolveInfo rs) {
         if (type == REPORT) {
@@ -215,24 +206,6 @@ final FilenameFilter filterNotReported = new FilenameFilter() {
             Visual.toast(this, R.string.length_name_of_file);
             return;
         }
-        if (rs.activityInfo.packageName.equals("com.google.android.apps.docs")) {
-            if (mGoogleApiClient == null) {
-                // Create the API client and bind it to an instance variable.
-                // We use this instance as the callback for connection and connection
-                // failures.
-                // Since no account name is passed, the user is prompted to choose.
-                waitForDrive = true;
-                mGoogleApiClient = new GoogleApiClient.Builder(this)
-                        .addApi(Drive.API)
-                        .addScope(Drive.SCOPE_FILE)
-                        .addConnectionCallbacks(this)
-                        .addOnConnectionFailedListener(this)
-                        .build();
-            }
-            mGoogleApiClient.connect();
-            // Connect the client. Once connected, the camera is launched.
-            return;
-        }
         ComponentName cn = new ComponentName(rs.activityInfo.packageName, rs.activityInfo.name);
         Intent i = new Intent();
         if (type == MESSAGE || type == MESSAGE_FOR_GROUP) {
@@ -274,21 +247,21 @@ final FilenameFilter filterNotReported = new FilenameFilter() {
             }
         }
         i.setComponent(cn);
-            if (((type == MESSAGE || type == MESSAGE_FOR_GROUP) && !(etFile.getText() + ".SPEC").equals(getString(R.string.file_name_secure_msg)))
-                    || (type == BACKUP && !(etFile.getText() + ".SPEC").equals(getString(R.string.file_name_Backup_msg)))) {
-                File newPath = new File(getFilesDir(), etFile.getText() + ".SPEC");
-                if (newPath.exists())
-                    newPath.delete();
-                File oldPath;
-                if (type == BACKUP)
-                    oldPath = new File(getFilesDir() + FilesManagement.MESSAGES, getString(R.string.file_name_Backup_msg));
-                else
-                    oldPath = new File(getFilesDir() + FilesManagement.MESSAGES, getString(R.string.file_name_secure_msg));
-                oldPath.renameTo(newPath);
-                uris = getUriForFile(this, getPackageName(), newPath);
-            }
-            i.setAction(Intent.ACTION_SEND);
-            grantUriPermission(cn.getPackageName(), uris, Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        if (((type == MESSAGE || type == MESSAGE_FOR_GROUP) && !(etFile.getText() + ".SPEC").equals(getString(R.string.file_name_secure_msg)))
+                || (type == BACKUP && !(etFile.getText() + ".SPEC").equals(getString(R.string.file_name_Backup_msg)))) {
+            File newPath = new File(getFilesDir(), etFile.getText() + ".SPEC");
+            if (newPath.exists())
+                newPath.delete();
+            File oldPath;
+            if (type == BACKUP)
+                oldPath = new File(getFilesDir() + FilesManagement.MESSAGES, getString(R.string.file_name_Backup_msg));
+            else
+                oldPath = new File(getFilesDir() + FilesManagement.MESSAGES, getString(R.string.file_name_secure_msg));
+            oldPath.renameTo(newPath);
+            uris = getUriForFile(this, getPackageName(), newPath);
+        }
+        i.setAction(Intent.ACTION_SEND);
+        grantUriPermission(cn.getPackageName(), uris, Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
         i.setType("file/spec");
         i.putExtra(Intent.EXTRA_STREAM, uris);
@@ -301,14 +274,8 @@ final FilenameFilter filterNotReported = new FilenameFilter() {
         }
     }
 
-private boolean waitForDrive = false;
-private GoogleApiClient mGoogleApiClient;
-
     @Override
     public void onPause() {
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.disconnect();
-        }
         super.onPause();
         new KeysDeleter(this);
     }
@@ -317,8 +284,7 @@ private GoogleApiClient mGoogleApiClient;
         loadIcons();
         if (type == REPORT) {
             ((TextView) findViewById(R.id.title_file)).setText("Share stack trace files VIA...");
-        }
-        else if (type == OPEN_FILE) {
+        } else if (type == OPEN_FILE) {
             ((TextView) findViewById(R.id.title_file)).setText(R.string.open_by_external_app);
         }
     }
@@ -355,99 +321,5 @@ private GoogleApiClient mGoogleApiClient;
             finish();
         } else
             KeysDeleter.stop();
-        if (waitForDrive) {
-            if (mGoogleApiClient == null) {
-                // Create the API client and bind it to an instance variable.
-                // We use this instance as the callback for connection and connection
-                // failures.
-                // Since no account name is passed, the user is prompted to choose.
-                mGoogleApiClient = new GoogleApiClient.Builder(this)
-                        .addApi(Drive.API)
-                        .addScope(Drive.SCOPE_FILE)
-                        .addConnectionCallbacks(this)
-                        .addOnConnectionFailedListener(this)
-                        .build();
-            }
-            // Connect the client. Once connected, the camera is launched.
-            mGoogleApiClient.connect();
-        }
-    }
-
-    private void saveFileToDrive() {
-        // Start by creating a new contents, and setting a callback.
-        Drive.DriveApi.newContents(mGoogleApiClient).setResultCallback(new ResultCallback<DriveApi.ContentsResult>() {
-
-            @Override
-            public void onResult(DriveApi.ContentsResult result) {
-                // If the operation was not successful, we cannot do anything
-                // and must
-                // fail.
-                if (!result.getStatus().isSuccess()) {
-                    return;
-                }
-                OutputStream outputStream = result.getContents().getOutputStream();
-                // Write the bitmap dataRaw from it.
-                try {
-                    outputStream.write(StaticVariables.dataRaw);
-                } catch (IOException ignored) {
-                }
-                // Create the initial metadata - MIME type and title.
-                // Note that the user will be able to change the title later.
-                MetadataChangeSet metadataChangeSet = new MetadataChangeSet.Builder()
-                        .setMimeType("file/spec").setTitle(getString(R.string.file_name_secure_msg)).build();
-                // Create an intent for the file chooser, and start it.
-                IntentSender intentSender = Drive.DriveApi
-                        .newCreateFileActivityBuilder()
-                        .setInitialMetadata(metadataChangeSet)
-                        .setInitialContents(result.getContents())
-                        .build(mGoogleApiClient);
-                try {
-                    startIntentSenderForResult(
-                            intentSender, 2, null, 0, 0, 0);
-                } catch (IntentSender.SendIntentException ignored) {
-                }
-            }
-        });
-    }
-
-    @Override
-    public void onConnected(Bundle bundle) {
-        Log.i("", "API client connected.");
-        saveFileToDrive();
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
-        Log.i("", "GoogleApiClient connection suspended");
-    }
-
-    @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i("", "GoogleApiClient connection failed: " + connectionResult.toString());
-        if (!connectionResult.hasResolution()) {
-            // show the localized error dialog.
-            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this, 0).show();
-            return;
-        }
-        // The failure has a resolution. Resolve it.
-        // Called typically when the app is not yet authorized, and an
-        // authorization
-        // dialog is displayed to the user.
-        try {
-            connectionResult.startResolutionForResult(this, 3);
-        } catch (Exception e) {
-            Log.e("", "Exception while starting resolution activity", e);
-        }
-    }
-
-    @Override
-    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        //DriveId i = data.getParcelableExtra(
-        //                OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
-        waitForDrive = false;
-        if (resultCode == RESULT_OK) {
-            Visual.toast(this, R.string.file_saved_drive);
-        }
-
     }
 }
